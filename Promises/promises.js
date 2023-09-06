7b4bb654cf0790b86626357adbb1f4cc1d278b34
peninah98 <peninangizwena@gmail.com>
2023-09-06 08:44:44 +0200

    Catch and then

--- Promises/promises.js
+++ Promises/promises.js
@@ -1,31 +1,42 @@
 const container = document.querySelector(".container");
 
-const getTodos = (resource, callback) => {
-  const request = new XMLHttpRequest();
+const getTodos = (resource) => {
+    return new Promise((resolve,reject)=>{
+        const request = new XMLHttpRequest();
 
-  request.addEventListener("readystatechange", () => {
-    // console.log(request, request.readyState)
-    if (request.readyState === 4 && request.status === 200) {
-      const data = JSON.parse(request.responseText);
-      callback(undefined, data);
-    } else if (request.readyState === 4) {
-      callback("Could not fetch data", undefined);
-    }
-  });
+        request.addEventListener("readystatechange", () => {
+          // console.log(request, request.readyState)
+          if (request.readyState === 4 && request.status === 200) {
+            const data = JSON.parse(request.responseText);
+            resolve( data);
+          } else if (request.readyState === 4) {
+            reject("Error,Could not fetch data");
+          }
+        });
 
-  request.open("GET", resource);
-  request.send();
+        request.open("GET", resource);
+        request.send();
+
+    })
+  
 };
+
+getTodos('../todos.json').then(data=>{
+    console.log('Resolved',data[1]);
+
+}).catch((err)=>{
+    console.log("Error",err)
+})
 
 // promies example
 
-const getsomething = () =>{
-    return new Promise((resolve,reject)=>{
-        //fetch somedata
-        resolve('some data')
-        reject("Some error") 
-    })
-}
+// const getsomething = () =>{
+//     return new Promise((resolve,reject)=>{
+//         //fetch somedata
+//         resolve('some data')
+//         reject("Some error") 
+//     })
+// }
 
 
 // getsomething().then((data) =>{
@@ -35,8 +46,8 @@ const getsomething = () =>{
 // })
 
 
-getsomething().then(data=>{
-    console.log(data)
-}).catch(err =>{
-    console.log(err)
-})
+// getsomething().then(data=>{
+//     console.log(data)
+// }).catch(err =>{
+//     console.log(err)
+// })

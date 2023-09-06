37a568ca0af1624ec15dda0c5d17a189923c23b1
peninah98 <peninangizwena@gmail.com>
2023-09-06 14:36:08 +0200

    Error throwing

--- fetching data/fetchdata.js
+++ fetching data/fetchdata.js
@@ -1,14 +1,40 @@
-const header = document.querySelector("#fetchingheader");
+// const header = document.querySelector("#fetchingheader");
 
-header.style.color = 'red'
+// header.style.color = 'red'
 
-fetch('../todos.json').then((response)=>{
-    console.log("Resolved", response)
-    return response.json()
+// fetch('../todos.json').then((response)=>{
+//     console.log("Resolved", response)
+//     return response.json()
 
-}).then((data)=>{
-    console.log(data)
-}).catch((err)=>{
-    console.log("Error", err)
+// }).then((data)=>{
+//     console.log(data)
+// }).catch((err)=>{
+//     console.log("Error", err)
 
-})
+// })
+
+
+const getTodos= async() =>{
+  const response = await fetch('../toxdos.json')
+  if(response.status !== 200){
+    throw new Error('Something went wrong!') 
+  }
+  const data = await response.json()
+  return data
+  
+}
+
+
+
+getTodos()
+    .then((data)=> console.log('Resolved', data[2]))
+    .catch((err)=>console.log('Rejected ',err.message))
+ 
+
+
+
+
+
+
+
+
